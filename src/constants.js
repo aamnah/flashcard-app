8e2b985190617a231d8db28b0917798c57b9843d
Aamnah <aamnah@aamnah.com>
2017-01-05 17:25:15 +0500

    + constants

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+// Cards
+export const ADD_CARD = 'ADD_CARD'
+
+// Decks
+export const ADD_DECK = 'ADD_DECK'
+export const SHOW_ADD_DECK = 'SHOW_ADD_DECK'
+export const HIDE_ADD_DECK = 'HIDE_ADD_DECK'
