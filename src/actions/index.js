95aa7e83ecfccc75abaae1dfd264ac9095581a01
Aamnah <aamnah@aamnah.com>
2017-01-05 17:52:47 +0500

    export actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,6 @@ import {
 } from '../constants'
 
 // Action Creators
-const addDeck = (name) => ({ type: ADD_DECK, data: name })
-const showAddDeck = () => ({ type: SHOW_ADD_DECK })
-const hideAddDeck = () => ({ type: HIDE_ADD_DECK })
+export const addDeck = (name) => ({ type: ADD_DECK, data: name })
+export const showAddDeck = () => ({ type: SHOW_ADD_DECK })
+export const hideAddDeck = () => ({ type: HIDE_ADD_DECK })
