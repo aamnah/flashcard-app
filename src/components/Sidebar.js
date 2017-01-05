7c3ebd19e7dec80962e0fcc771a580527f70e31b
Aamnah <aamnah@aamnah.com>
2017-01-05 17:24:57 +0500

    + Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,23 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class Sidebar extends Component {
+  render () {
+    const { decks, addingDeck } = this.props
+    
+    return (
+      <div className='sidebar'>
+        <h2>All Decks</h2>
+        <ul>
+          { decks.map(deck => <li key={deck.name}>{deck.name}</li>) }
+        </ul>
+        { addingDeck && <input ref='add' /> } 
+      </div>
+    )
+
+  }
+}
+
+Sidebar.propTypes = {
+  addingDeck: PropTypes.bool,
+  decks: PropTypes.array
+}
