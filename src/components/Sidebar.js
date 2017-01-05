1f3d78be9c6d6a19850ff85536fabe21445410ef
Aamnah <aamnah@aamnah.com>
2017-01-05 17:53:07 +0500

    WIP: adding actions to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,33 @@
 import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
 
 export default class Sidebar extends Component {
+  
+  createDeck (e) {
+    const { addDeck, hideAddDeck } = this.props
+    
+    if (e.which !== 13) return
+
+    const name = ReactDOM.findDOMNode(this.refs.add)
+    addDeck(name)
+    hideAddDeck()
+  }
+  
   render () {
-    const { decks, addingDeck } = this.props
+    const { decks, addingDeck, showAddDeck } = this.props
     
     return (
       <div className='sidebar'>
         <h2>All Decks</h2>
+        
+        <button onClick={e => showAddDeck }>New Deck</button>
+
         <ul>
           {decks.map(deck => 
             <li key={deck.name}>{deck.name}</li>
           )}
         </ul>
-        {addingDeck && <input ref='add' />} 
+        {addingDeck && <input ref='add' onKeyPress={this.createDeck}/>} 
       </div>
     )
 
